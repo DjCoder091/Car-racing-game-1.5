5314995d1888c74862b3688c520d3ecf875685e9
DjCoder091 <71996904+DjCoder091@users.noreply.github.com>
2020-12-05 14:11:11 -0800

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,36 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+2
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      players.name = this.input.value();
+      playerCount+=1;
+      players.index = playerCount;
+      players.update();
+      players.updateCount(playerCount);
+      this.greeting.html("Hello " + players.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+  }
+}
